efa314cc5531e8e877942849d35142ac80746dbb
hanseunghun <momuse723@gmail.com>
2025-04-04 11:44:39 +0900

    fix: Add missing make_subplots import

--- simple_app.py
+++ simple_app.py
@@ -5,6 +5,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from datetime import datetime, timedelta
 import numpy as np
 
