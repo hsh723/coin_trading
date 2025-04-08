f00a12fb1fb04a7cc7ac89829727f0d549e4e26c
hanseunghun <momuse723@gmail.com>
2025-04-08 09:19:40 +0900

    streamlit_app.py: nest_asyncio 모듈 import 추가

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ import asyncio
 from typing import Dict, List, Any, Optional
 import sys
 from dotenv import load_dotenv
+import nest_asyncio
 
 from src.analysis.technical_analyzer import TechnicalAnalyzer
 from src.analysis.self_learning import SelfLearningSystem
